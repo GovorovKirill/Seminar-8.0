bb97f0ce0aa2778e7098a11de5734aed35a0c882
Kirill <kirillgovorov@gmail.com>
2023-08-03 23:47:28 +0300

    Task 56 почти  is done

---
+++ Task56/Program.cs
@@ -0,0 +1,93 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],3}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+// void SumElemRows (int[,] matrix)
+// {
+
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         int sum = 0;
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//            sum = sum + matrix[i,j]; 
+//         }
+//     }
+// }
+
+int[] CreateArraySumElemRow(int[,] matrix)
+{
+    int[] array = new int[matrix.GetLength(0)];
+    int k = 0;
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum = sum + matrix[i, j];
+        }
+
+        array[k] = sum;
+        k++;
+    }
+    return array;
+}
+
+void PrintArray(int[] arr);
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length-1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+
+
+
+
+
+
+int[,] array2D = CreateMatrixRndInt(3, 4, -10, 10);
+PrintMatrix(array2D);
+// SumElemRows(array2D);
+int[] arrSumElemRow = CreateArraySumElemRow(array2D);
+PrintArray(array2D);
